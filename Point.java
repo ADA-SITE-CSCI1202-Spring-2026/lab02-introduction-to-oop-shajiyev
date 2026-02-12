c8400be46a3d19b8d4effbaaac6d464b638c0b5d
Sabir Hajiyev <sabir.haciyev613@gmail.com>
2026-02-12 11:08:35 +0400

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,38 @@
+package week03.geometry;
+
+public class Point {
+    private float x;
+    private float y;
+
+    public Point() {
+        x = 0f;
+        y = 0f;
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+    
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
+
